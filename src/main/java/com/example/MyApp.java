787e0395b3767237bc53fc2c4dce8e5ec89bdfaa
Anna Rzucidło <arzucidlo1@gmail.com>
2020-01-12 17:33:20 +0100

    Finished

--- src/main/java/com/example/MyApp.java
+++ src/main/java/com/example/MyApp.java
@@ -5,11 +5,19 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class MyApp {
 
     public static void main(String[] args) {
+
         ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
 
         MessageService messageService = applicationContext.getBean("messageService", MessageService.class);
+        MessageService secondmessageService = applicationContext.getBean("RandomMessageService", MessageService.class);
 
         System.out.println(messageService.getMessage());
+        System.out.println(secondmessageService.getMessage());
+
+        System.out.println(messageService.hashCode());
+        System.out.println(secondmessageService.hashCode());
+
+
 
         applicationContext.close();
     }
